1344c8105fb72d93f1959de645ad7dda60ac2c7e
jraduege <git@lvec.org>
2022-03-29 07:13:31 -0700

    Draw line with mouseover

--- sketch.js
+++ sketch.js
@@ -6,9 +6,17 @@ for (let i=1; i<= resolution; i++) {
     currentRow.classList.add("row");
     for (let j=1; j<=resolution; j++) {
         let currentDot= document.createElement("div");
-        currentDot.classList.add("dot", `data-x=${j}`, `data-y=${i}`);
+        currentDot.classList.add("dot");
         currentDot.textContent = `${j},${i}`;
         currentRow.appendChild(currentDot);
     }
     container.appendChild(currentRow);
+}
+
+const dots=document.querySelectorAll(".dot");
+dots.forEach((dot) => dot.addEventListener("mouseover",handleHover));
+
+function handleHover(e) {
+    dot = e.target;
+    dot.classList.add("active");
 }
